1c5e37b92f33676cbc38d9138776a3a7b9b96439
SnippyRex <kentf022@gmail.com>
2023-04-10 22:19:57 +0100

    Build: Add drawable as app banner fit to the device's width

--- ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
@@ -23,6 +27,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                    BannerImage()
                 }
             }
         }
@@ -33,6 +38,15 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
+
+@Composable
+fun BannerImage(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.banner)
+    Box {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.FillWidth)
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
