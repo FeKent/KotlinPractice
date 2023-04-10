b376779f5e00a91e96762420229f407528e71f4b
SnippyRex <kentf022@gmail.com>
2023-04-10 22:53:59 +0100

    Build: Add softcode strings to preview in a basic unfinished layout

--- ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,35 +28,62 @@ class MainActivity : ComponentActivity() {
             ComposeArticleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Article(
+                        title = stringResource(R.string.title_text),
+                        intro = stringResource(R.string.intro_text),
+                        main = stringResource(R.string.main_text)
+                    )
                     BannerImage()
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Composable
 fun BannerImage(modifier: Modifier = Modifier) {
     val image = painterResource(id = R.drawable.banner)
     Box {
-        Image(painter = image, contentDescription = null, contentScale = ContentScale.FillWidth)
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
     }
 }
 
+@Composable
+fun Article(title: String, intro: String, main: String, modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = intro,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = main,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        Article(
+            title = stringResource(R.string.title_text),
+            intro = stringResource(R.string.intro_text),
+            main = stringResource(R.string.main_text)
+        )
+        BannerImage()
     }
 }
