fc149efbfde5c36f8777b7a353bde0ba5d650094
SnippyRex <kentf022@gmail.com>
2023-04-12 22:40:27 +0100

    Build: Add other icons and paired details, change stylisation and spacing

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.businesscard
 
 import android.graphics.Color.parseColor
+import android.graphics.drawable.Icon
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -13,17 +14,23 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -74,17 +81,32 @@ fun Intro(name: String, title: String, modifier: Modifier = Modifier) {
             contentDescription = "Picture of a woman",
             contentScale = ContentScale.FillWidth
         )
-        Text(text = name)
-        Text(text = title)
+        Text(text = name, color = Color.White, fontSize = 36.sp, fontWeight = FontWeight.Light)
+        Text(text = title, fontWeight = FontWeight.Bold ,color = Color(parseColor("#3DDC84")))
         Row(modifier = Modifier.padding(top = 50.dp)) {
             Icon(
-                Icons.Rounded.Share,
+                Icons.Rounded.Phone,
+                contentDescription = "Phone Icon",
+                modifier = Modifier.padding(end = 15.dp)
+            )
+            Text(text = "07850 122781", color = Color.LightGray)
+        }
+        Row() {
+            Icon(
+                imageVector = Icons.Rounded.Share,
                 contentDescription = "Share Link Icon",
                 modifier = Modifier.padding(end = 15.dp)
             )
-            Text(text = "https://github.com/SnippyRex")
+            Text(text = "https://github.com/SnippyRex", color = Color.LightGray)
         }
-
+        Row() {
+            Icon(
+                Icons.Rounded.Email,
+                contentDescription = "Mail Icon",
+                modifier = Modifier.padding(end = 15.dp)
+            )
+            Text(text = "kentf022@gmail.com", color = Color.LightGray)
+        }
     }
 }
 
