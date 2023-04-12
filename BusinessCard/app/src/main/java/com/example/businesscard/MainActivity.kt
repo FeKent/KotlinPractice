ce48874314213c99b0e3704a42cd7befd93688ee
SnippyRex <kentf022@gmail.com>
2023-04-12 21:56:55 +0100

    Build: Set background color, add Share icon and partnered link

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.businesscard
 
+import android.graphics.Color.parseColor
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -30,8 +33,8 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
                     Intro(
                         name = stringResource(R.string.name),
@@ -52,21 +55,36 @@ fun Greeting(name: String) {
 @Composable
 fun Intro(name: String, title: String, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                color = Color(parseColor("#073042"))
+            ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.android_logo),
-            contentDescription = null,
-            modifier = Modifier.size(120.dp).padding(top = 20.dp, bottom = 50.dp)
+            contentDescription = "Android Logo",
+            modifier = Modifier
+                .size(120.dp)
+                .padding(top = 20.dp, bottom = 50.dp)
         )
         Image(
             painter = painterResource(id = R.drawable.headshot),
-            contentDescription = null,
+            contentDescription = "Picture of a woman",
             contentScale = ContentScale.FillWidth
         )
         Text(text = name)
         Text(text = title)
+        Row(modifier = Modifier.padding(top = 50.dp)) {
+            Icon(
+                Icons.Rounded.Share,
+                contentDescription = "Share Link Icon",
+                modifier = Modifier.padding(end = 15.dp)
+            )
+            Text(text = "https://github.com/SnippyRex")
+        }
+
     }
 }
 
